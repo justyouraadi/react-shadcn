ace21abd1e1e28d1b1080487dcee23d1afd2133e
justyouraddi <aadityakumawat111@gmail.com>
2026-02-13 17:42:52 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Toaster } from 'sonner'
 
 createRoot(document.getElementById('root')).render(
+    
+   <>
     <App />
+    <Toaster richColors />
+   </>
+
 )
